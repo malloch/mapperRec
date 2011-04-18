c4b1bd8b789fca33f092e2e4138d2d0ac94b250d
Joseph Malloch <joseph.malloch@mcgill.ca>
2011-04-18 16:33:46 -0400

    Added timetags to signal handlers to match libmapper modification.

--- recdevice.c
+++ recdevice.c
@@ -14,7 +14,7 @@ struct
     int frame;
 } framestamp = { 0, 0 };
 
-static void frame_handler(mapper_signal msig, mapper_db_signal props, void *v)
+static void frame_handler(mapper_signal msig, mapper_db_signal props, mapper_timetag_t *time, void *v)
 {
     int *i = (int*)v;
     framestamp.take  = i[0];
@@ -46,7 +46,7 @@ void recdevice_stop()
         mdev_free(recdev);
 }
 
-void input_handler(struct _mapper_signal *msig, mapper_db_signal props, void *v)
+void input_handler(struct _mapper_signal *msig, mapper_db_signal props, mapper_timetag_t *time, void *v)
 {
     backend_write_value(msig, v);
 }
